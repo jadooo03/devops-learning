6b24b3f29db40055061aeabb3c9d759645132a6d
jadooo03 <ayushanandbourai@gmail.com>
2025-11-24 22:31:22 +0530

    Initial commit for Docker learning files

---
+++ docket/app.py
@@ -0,0 +1,14 @@
+#app.py 
+
+from flask import Flask
+
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return "Hello from inside a docker container!"
+
+if __name__ == "__main__":
+    app.run(debug=True,host='0.0.0.0',port=8080)
