04b717e22eefdd6e504b56761573eca32d8de5a4
jadooo03 <ayushanandbourai@gmail.com>
2025-12-14 16:12:49 +0530

    adding 2 private subnets, private rt, elastic ip and 2 private assocs

--- terraform/eks-terraform/main.tf
+++ terraform/eks-terraform/main.tf
@@ -50,9 +50,9 @@ resource "aws_subnet" "public-2" {
 
 resource "aws_route_table" "public_rt" {
   vpc_id = aws_vpc.my_eks_vpc.id
-  route = {
+  route {
     cidr_block = "0.0.0.0/0"
-    aws_internet_gateway = aws_internet_gateway.my_igw.id
+    gateway_id = aws_internet_gateway.my_igw.id
   }
   tags = {
     Name = "public-route-table"
@@ -67,4 +67,69 @@ resource "aws_route_table_association" "pub-1-assc" {
 resource "aws_route_table_association" "pub-2-assc" {
   subnet_id = aws_subnet.public-2.id
   route_table_id = aws_route_table.public_rt.id
+}
+
+resource "aws_eip" "nat_eip" {
+  domain = "vpc"
+  tags = {
+    Name = "my-eks-nat-eip"
+  }
+}
+
+resource "aws_nat_gateway" "my_nat" {
+  allocation_id = aws_eip.nat_eip.id
+  subnet_id = aws_subnet.public-1.id
+
+  tags = {
+    Name = "my-eks-nat"
+  }
+
+  depends_on = [ aws_internet_gateway.my_igw ]
+}
+
+resource "aws_subnet" "private_1" {
+  vpc_id = aws_vpc.my_eks_vpc.id
+  cidr_block = "10.0.3.0/24"
+  availability_zone = "ap-south-1a"
+
+  tags = {
+    Name = "private-subnet-1"
+    "kubernetes.io/cluster/my-first-eks-cluster"="shared"
+    "kubernetes.io/role/internal-elb"="1"
+  }
+}
+
+resource "aws_subnet" "private_2" {
+  vpc_id = aws_vpc.my_eks_vpc.id
+  cidr_block = "10.0.4.0/24"
+  availability_zone = "ap-south-1b"
+
+  tags = {
+    Name = "private-subnet-2"
+    "kubernetes.io/cluster/my-first-eks-cluster"="shared"
+    "kubernetes.io/role/internal-elb"="1"
+  }
+}
+
+resource "aws_route_table" "private_rt" {
+  vpc_id = aws_vpc.my_eks_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.my_nat.id
+  }
+
+  tags = {
+    Name = "private-route-table"
+  }
+}
+
+resource "aws_route_table_association" "priv_1_assoc" {
+  subnet_id = aws_subnet.private_1.id
+  route_table_id = aws_route_table.private_rt.id
+}
+
+resource "aws_route_table_association" "priv_2_assoc" {
+  subnet_id = aws_subnet.private_2.id
+  route_table_id = aws_route_table.private_rt.id
 }
